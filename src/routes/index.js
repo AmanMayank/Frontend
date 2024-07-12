e1bd6df2e2b80b74ca6b1a6d1e8a444efd185452
Aman Mayank <62825517+AmanMayank@users.noreply.github.com>
2024-07-12 09:33:32 +0530

    Initial setUp

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+]);
